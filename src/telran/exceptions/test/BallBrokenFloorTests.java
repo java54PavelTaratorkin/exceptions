c0f3129add60c40388bfd588b25db4d9e6a820c1
PaulT1983 <pault83@gmail.com>
2024-04-03 18:46:29 +0300

    Homework#12. BallBrokenFloorTests using binary search algorithm and exceptions.

--- src/telran/exceptions/test/BallBrokenFloorTests.java
+++ src/telran/exceptions/test/BallBrokenFloorTests.java
@@ -20,11 +20,22 @@ class BallBrokenFloorTests {
 	}
 
 	private int getMinBrokenFloor(BallBrokenFloor bbf) {
-		// TODO
-		// using binary search algorithm  and only method checkFloor 
+		// using binary search algorithm and only method checkFloor 
 		// of the class BallBrokenFloor
 		// you should find minimal floor, from which a ball will be broken
-		return -1;
+		int left = 1;
+		int right = N_FLOORS;
+		int middle = (left + right) / 2;
+		while(left < right) {			
+			try {
+				bbf.checkFloor(middle);
+				left = middle + 1;
+			} catch (Exception e) {
+				right = middle;
+			}
+			middle = (left + right) / 2;
+		}
+		return middle;
 	}
 	
 }
